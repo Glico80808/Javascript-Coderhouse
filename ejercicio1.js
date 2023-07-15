d19e4f1b2683bc0afb5f4805b99e3f5580836d20
Glico80808 <92232831+Glico80808@users.noreply.github.com>
2023-07-15 16:19:27 -0300

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,21 @@
+let entrada = prompt("Por favor, ingrese su nombre");
+if (entrada == ""){
+    alert( "El campo sigue vacío");
+}
+else {
+    alert("Nombre ingresado"); 
+}
+let entrada2 = prompt("Por favor, ingrese su apellido");
+if (entrada == ""){
+    alert( "El campo sigue vacío");
+}
+else {
+    alert("Apellido ingresado");
+}
+let entrada3 = prompt( " Por último, ingrese su año de nacimiento")
+let fecha = 2023;
+let nacimiento = parseInt(entrada3);
+let año = fecha - nacimiento;
+let nombrecompleto = entrada + " " + entrada2
+let salida = "Bienvenido/a"+ "  " + nombrecompleto + " de " + año + " " + "años";
+alert(salida);
