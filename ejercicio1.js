43094b35ad95dcb05c106dfe66e9f272035416da
Glico80808 <efrae@hotmail.com>
2023-07-15 16:54:42 -0300

    Primer ejercicio javascript (act)

--- ejercicio1.js
+++ ejercicio1.js
@@ -6,13 +6,22 @@ else {
     alert("Nombre ingresado"); 
 }
 let entrada2 = prompt("Por favor, ingrese su apellido");
-if (entrada == ""){
+if (entrada2 == ""){
     alert( "campo vacío");
 }
 else {
     alert("Apellido ingresado");
 }
 let entrada3 = prompt( " Por último, ingrese su año de nacimiento")
+if (entrada3 == ""){
+    alert( "campo vacío");
+}
+else if (entrada3 > 2022) {
+    alert( "ingrese una fecha valida")
+}
+else {
+    alert("fecha ingresada");
+}
 let fecha = 2023;
 let nacimiento = parseInt(entrada3);
 let año = fecha - nacimiento;
