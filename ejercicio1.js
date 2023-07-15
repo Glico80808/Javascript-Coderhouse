c92fb35d56df564fe5245aeff15b53e92b58e71b
Glico80808 <efrae@hotmail.com>
2023-07-15 18:16:36 -0300

    Primer ejercicio Javascript (act1)

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,26 +1,28 @@
 let entrada = prompt("Por favor, ingrese su nombre");
-if (entrada == ""){
-    alert( "campo vacío");
+
+while (entrada == "") {
+    alert ("Campo vacío");
+    entrada = prompt("Por favor, ingrese su nombre");
 }
-else {
-    alert("Nombre ingresado"); 
+if (entrada != ""){
+    alert( "Nombre ingresado");
 }
+   
 let entrada2 = prompt("Por favor, ingrese su apellido");
-if (entrada2 == ""){
-    alert( "campo vacío");
+while (entrada2 == "") {
+    alert ("Campo vacío");
+    entrada2 = prompt("Por favor, ingrese su apellido");
 }
-else {
-    alert("Apellido ingresado");
+if (entrada2 != ""){
+    alert( "Apellido ingresado");
 }
-let entrada3 = prompt( " Por último, ingrese su año de nacimiento")
-if (entrada3 == ""){
-    alert( "campo vacío");
+let entrada3 = prompt( " Por último, ingrese su año de nacimiento");
+while ((entrada3 == "") || (entrada3 > 2023)) {
+    alert ("Fecha inválida");
+    entrada3 = prompt("Por favor, ingrese su año de nacimiento");
 }
-else if (entrada3 > 2022) {
-    alert( "ingrese una fecha valida")
-}
-else {
-    alert("fecha ingresada");
+if ((entrada3 != "") && ( entrada3 < 2023)){
+    alert( "Fecha ingresada");
 }
 let fecha = 2023;
 let nacimiento = parseInt(entrada3);
